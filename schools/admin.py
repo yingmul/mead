1946b43445ef30ed0aab44f488004a0359689196
Audrey Liu <yingmul.at.work@gmail.com>
2012-12-09 14:25:47 -0500

    Flushing out models for school app and admin tool

--- schools/admin.py
+++ schools/admin.py
@@ -1,4 +1,35 @@
-from schools.models import School
+from schools.models import School, Class, SchoolAddress, Schedule, Instructor
 from django.contrib import admin
 
-admin.site.register(School)
+class AddressInline(admin.StackedInline):
+    model = SchoolAddress
+    extra = 1
+
+
+class ScheduleInline(admin.TabularInline):
+    model = Schedule
+    extra = 1
+
+
+class InstructorInline(admin.StackedInline):
+    model = Instructor
+    extra = 1
+
+
+class InstructorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'website')
+
+
+class SchoolAdmin(admin.ModelAdmin):
+    inlines = [AddressInline]
+    list_display = ('name', 'website')
+
+
+class ClassAdmin(admin.ModelAdmin):
+    inlines = [ScheduleInline]
+    list_display = ('school', 'name', 'price')
+
+
+admin.site.register(School, SchoolAdmin)
+admin.site.register(Class, ClassAdmin)
+admin.site.register(Instructor, InstructorAdmin)
--- schools/models.py
+++ schools/models.py
@@ -5,7 +5,10 @@ from django.contrib.localflavor.us.models import USStateField
 
 class School(models.Model):
     name = models.CharField(max_length=200)
+    website = models.URLField(blank=True, null=True)
 
+    def __unicode__(self):
+        return self.name
 
 class SchoolAddress(models.Model):
     school = models.ForeignKey(School, related_name="addresses")
@@ -14,12 +17,48 @@ class SchoolAddress(models.Model):
     city = models.CharField('City', max_length=100)
     state = USStateField('State')
     postal_code = models.CharField('Zip Code', max_length=10, null=True)
-    phone_number = PhoneNumberField('Phone Number')  # mobile_phone (optional)
+    phone_number = PhoneNumberField('Phone Number', blank=True, null=True)  # mobile_phone (optional)
 
     class Meta:
-        verbose_name = 'Profile Address'
-        verbose_name_plural = 'Profile Addresses'
+        verbose_name = 'School Address'
+        verbose_name_plural = 'School Addresses'
 
     def __unicode__(self):
         return "Address for {0}".format(self.school.name)
 
+
+class Class(models.Model):
+    school = models.ForeignKey(School, related_name='classes')
+    name = models.CharField(max_length=200)
+    code = models.CharField(max_length=20, blank=True, null=True)
+    description = models.TextField(blank=True, default='')
+    price = models.DecimalField(max_digits=11, decimal_places=2, default=0)
+    # TODO: add status: open, wait list, only 2 left
+
+    class Meta:
+        verbose_name_plural = 'Classes'
+
+    def __unicode__(self):
+        return "Class {0} for {1}".format(self.name, self.school.name)
+
+
+class Schedule(models.Model):
+    school_class = models.ForeignKey(Class, related_name="class_schedules")
+    start_date = models.DateField('Start Date')
+    num_meetings = models.IntegerField(default=1)
+    start_time = models.DateTimeField('Start Time')
+    end_time = models.DateTimeField('End Time')
+    max_students = models.IntegerField(null=True)
+
+    def __unicode__(self):
+        return 'Schedule {0} for {1}'.format(self.start_date, self.school_class.name)
+
+
+class Instructor(models.Model):
+    schedule = models.ForeignKey(Schedule, related_name="instructors")
+    name = models.CharField(max_length=50)
+    bio = models.TextField(blank=True, default='')
+    website = models.URLField(blank=True, null=True)
+
+    def __unicode__(self):
+        return self.name
