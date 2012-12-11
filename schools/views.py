124afa7d3cafc1ff9648f6cd046d8acc524006b1
Audrey Liu <yingmul.at.work@gmail.com>
2012-12-10 23:51:39 -0500

    display all schools

--- schools/views.py
+++ schools/views.py
@@ -1,8 +1,13 @@
 from django.template import Context, loader
 from django.http import HttpResponse
 
+from schools.models import School
+
 #TODO make this into a view class, change schools/index.html to use sitebase
 def index(request):
-    c = Context({})
+    schools = School.objects.all()
+    context = Context({
+        'schools': schools
+    })
     t = loader.get_template('schools/index.html')
-    return HttpResponse(t.render(c))
+    return HttpResponse(t.render(context))
